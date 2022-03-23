469bc0410a1fcd1334bb953a4330fd93597201f2
DariaAzanovich <azanovich.dasha@gmail.com>
2022-03-23 17:57:03 +0300

    RA-20 add single page

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <p>Hello World of React and Webpack as a bundler!</p>
+    </div>
+  );
+};
+
+export default Home;
