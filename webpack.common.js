313a7fb2294cc8c52ed994c929b8867e2ea19885
DariaAzanovich <azanovich.dasha@gmail.com>
2022-07-04 16:50:38 +0300

    RA-48: add prod mode

--- webpack.common.js
+++ webpack.common.js
@@ -2,15 +2,11 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const port = process.env.PORT || 3000;
-
 module.exports = {
-  mode: 'development',
   entry:  path.join(__dirname, "src", "index.js"),
   output: {
     path: path.resolve(__dirname, "dist")
   },
-  devtool: 'inline-source-map',
   module: {
     rules: [
       {
@@ -44,10 +40,4 @@ module.exports = {
       favicon: path.join(__dirname, "public", "favicon.ico")
     })
   ],
-  devServer: {
-    host: 'localhost',
-    port: port,
-    historyApiFallback: true,
-    open: true
-  }
 };
